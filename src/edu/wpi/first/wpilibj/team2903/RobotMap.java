c26e1191dc7ab2dcb6caa5965a7e34aafcb269e4
artleung123 <artleung2468@gmail.com>
2014-01-28 16:48:53 -0800

    Added motors to robot map

--- src/edu/wpi/first/wpilibj/team2903/RobotMap.java
+++ src/edu/wpi/first/wpilibj/team2903/RobotMap.java
@@ -9,6 +9,8 @@ package edu.wpi.first.wpilibj.team2903;
 public class RobotMap {
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
+    
+    //moduale 1
     public static final int leftFrontMotor1 = 1;
     public static final int leftFrontMotor2 = 2;
     public static final int rightFrontMotor1 = 3;
@@ -17,6 +19,11 @@ public class RobotMap {
     public static final int leftBackMotor2 = 6;
     public static final int rightBackMotor1 = 7;
     public static final int rightBackMotor2 = 8;
+    //module 2
+    public static final int dstickHeight = 1;
+    public static final int dstickSlide = 2;
+    public static final int dstickLeftTrack = 3;
+    public static final int dstickRightTrack = 4;
     
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
