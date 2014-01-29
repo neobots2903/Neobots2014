7d43aedc83982a1a3d1899f8c036071954f86299
robot <robot@HS-CAD-WIN7-19.hsstudent.arlington.asd>
2014-01-28 17:19:44 -0800

    Added class BallManager

---
+++ src/edu/wpi/first/wpilibj/team2903/subsystems/BallManager.java
@@ -0,0 +1,19 @@
+package edu.wpi.first.wpilibj.team2903.subsystems;
+//Z and locked button
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.team2903.RobotMap;
+/**
+ *
+ * @author robot
+ */
+public class BallManager  extends Subsystem{
+       
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+        
+    
+}
