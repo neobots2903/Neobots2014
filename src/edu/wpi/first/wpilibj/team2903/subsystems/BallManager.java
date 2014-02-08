8643345c111a8a5c604adbffbb7dadc75d8a1f1b
groot314 <groot314@gmail.com>
2014-02-07 16:49:48 -0800

    added capture function

--- src/edu/wpi/first/wpilibj/team2903/subsystems/BallManager.java
+++ src/edu/wpi/first/wpilibj/team2903/subsystems/BallManager.java
@@ -12,11 +12,20 @@ import edu.wpi.first.wpilibj.team2903.RobotMap;
 public class BallManager extends Subsystem {
 
     public Victor launcherMotor = new Victor(RobotMap.launcher);
-    //TODO: catcher motor
+    public Victor captureMotor = new Victor(RobotMap.catcher);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public boolean capture(boolean capture){
+        if(capture){
+            captureMotor.setPosition(720);//TODO: Get correct position
+        }else{
+            captureMotor.setPosition(0);
+        }
+        return capture;
     }
 
     public void Shoot() throws InterruptedException {
