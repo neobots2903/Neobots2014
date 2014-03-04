542272767b76f9070fc526e1d49db9fe94fc36fb
groot314 <groot314@gmail.com>
2014-03-03 21:13:09 -0800

    Added thread for drive

--- src/edu/wpi/first/wpilibj/team2903/commands/TeleopController.java
+++ src/edu/wpi/first/wpilibj/team2903/commands/TeleopController.java
@@ -28,64 +28,65 @@ public class TeleopController extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if (!stop) {
-
-            if (forward) {
-                drive.drive(-OI.gameController.getRawAxis(3), OI.gameController.getRawAxis(2), -OI.gameController.getRawAxis(1) * 1.2, multi);
-            } else {
-                drive.drive(OI.gameController.getRawAxis(3), -OI.gameController.getRawAxis(2), OI.gameController.getRawAxis(1) * 1.2, multi);
+        Thread driveT = new Thread(new Runnable() {
+            public void run() {
+                if (forward) {
+                    drive.drive(-OI.gameController.getRawAxis(3), OI.gameController.getRawAxis(2), -OI.gameController.getRawAxis(1) * 1.2, multi);
+                } else {
+                    drive.drive(OI.gameController.getRawAxis(3), -OI.gameController.getRawAxis(2), OI.gameController.getRawAxis(1) * 1.2, multi);
+                }
             }
+        });
+        driveT.start();
+        drive.leftStrafe(OI.gameController.getRawButton(5), multi);
+        drive.rightStrafe(OI.gameController.getRawButton(6), multi);
 
-            drive.leftStrafe(OI.gameController.getRawButton(5), multi);
-            drive.rightStrafe(OI.gameController.getRawButton(6), multi);
-
-            if (OI.gameController.getRawButton(2)) {
-                multi = 0.25;
-                //System.out.println(multi);
-            } else if (OI.gameController.getRawButton(4)) {
-                multi = 0.5;
-                //System.out.println(multi);
-            } else if (OI.gameController.getRawButton(3)) {
-                multi = 0.75;
-                //System.out.println(multi);
-            } else if (OI.gameController.getRawButton(1)) {
-                multi = 1;
-                //System.out.println(multi);
-            } else if (OI.gameController.getRawButton(0)) {//TODO: Find button for this
-                forward = !forward;
-            }
-            
-            if (OI.gameController.getRawButton(8)){
-            
-                Ball.Shoot();
-                
-            }
-            
-            //2nd controller for defence
-            Defend.horizontal(OI.gameController2.getRawAxis(1));
-            Defend.vertical(OI.gameController2.getRawAxis(2));
-            Ball.hold(OI.gameController2.getRawAxis(3));
-            
-            
+        if (OI.gameController.getRawButton(2)) {
+            multi = 0.25;
+            //System.out.println(multi);
+        } else if (OI.gameController.getRawButton(4)) {
+            multi = 0.5;
+            //System.out.println(multi);
+        } else if (OI.gameController.getRawButton(3)) {
+            multi = 0.75;
+            //System.out.println(multi);
+        } else if (OI.gameController.getRawButton(1)) {
+            multi = 1;
+            //System.out.println(multi);
+        } else if (OI.gameController.getRawButton(0)) {//TODO: Find button for this
+            forward = !forward;
         }
-        if (OI.gameController.getRawButton(9) && stop) {
-            stop = false;
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                System.out.println(ex);
-            }
-        } else if (OI.gameController.getRawButton(9) && !stop) {
-            stop = true;
-            drive.drive(0, 0, 0, 0);
-            multi = 0;
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                System.out.println(ex);
-            }
+
+        if (OI.gameController.getRawButton(8)) {
+
+            Ball.Shoot();
 
         }
+
+        //2nd controller for defence
+        Defend.horizontal(OI.gameController2.getRawAxis(1));
+        Defend.vertical(OI.gameController2.getRawAxis(2));
+        Ball.hold(OI.gameController2.getRawAxis(3));
+
+
+//        if (OI.gameController.getRawButton(9) && stop) {
+//            stop = false;
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException ex) {
+//                System.out.println(ex);
+//            }
+//        } else if (OI.gameController.getRawButton(9) && !stop) {
+//            stop = true;
+//            drive.drive(0, 0, 0, 0);
+//            multi = 0;
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException ex) {
+//                System.out.println(ex);
+//            }
+//
+//        }
 
     }
 
